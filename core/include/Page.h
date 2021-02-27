d6e774b8480d3fbece28cb50d43bd14ecfd0ae98
Josh Gerber <gerber32@purdue.edu>
2021-02-27 10:52:57 -0500

    -Updated CMakeLists.txt for editor to not use core library and for core to run as executable. -Created Page.cpp/.h which only has a constructor that initializes the context/window and has an "example" render function for testing -Much of what I added to Game.cpp was example code from Editor.cpp but reworked to separate chunks of code away from main() and removed the ImGUI stuff. The first notable change would be the addition of sdl_die(msg), which will be useful when debugging the Game class. The other notable mention would be the few lines within the game loop in main(), which is simply a test to confirm that pages can correctly draw their render() functions (they can!)
    -TLDR; pages in their most basic state are added and can be "drawn" from the game class using Page::render()

---
+++ core/include/Page.h
@@ -0,0 +1,19 @@
+#include <SDL.h>
+
+// Page class
+// Pages can be menu, settings, HUD, map, Cutscene, ...
+// Each page contains its own context and render function
+class Page {
+
+public:
+	// Constructor, just creates the context of the page
+	Page(SDL_Window* window);
+
+	// Render the given page on the current context
+	// Returns non-zero on error
+	int render();
+
+private:
+	SDL_GLContext mPageContext; // The page context
+	SDL_Window* mWindow; // The window the page context is related to
+};
