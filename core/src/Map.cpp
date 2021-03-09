73a0b98d4e7e89a64aea96f2ff9dbb57e88a2dd6
Josh Gerber <gerber32@purdue.edu>
2021-03-09 15:41:43 -0500

    Allow page windows to be access from subclasses (just MapPage for now, but other pages will be added later). Also added a basic test to make sure maps can be set by spriteMaps correctly.

--- core/src/Map.cpp
+++ core/src/Map.cpp
@@ -5,16 +5,16 @@ Map::Map(glm::vec2 dimensions, int tileSize) :
 mMapDimensions(dimensions),
 mTileSize(tileSize) {
 	// Create map 
-	int numTiles = mMapDimensions.x * mMapDimensions.y;
-	mTileArray = new Tile[numTiles];
+	mNumTiles = mMapDimensions.x * mMapDimensions.y;
+	mTileArray = new Tile[mNumTiles];
 }
 
 // Set mMapDimensions to new dimensions
 void Map::setDimensions(glm::vec2 dimensions) {
 	// setDimensions should copy the map to the new map! right now it just discards the old one
 	delete mTileArray;
-	int numTiles = mMapDimensions.x * mMapDimensions.y;
-	Tile* newMap = new Tile[numTiles];
+	mNumTiles = mMapDimensions.x * mMapDimensions.y;
+	Tile* newMap = new Tile[mNumTiles];
 	mTileArray = newMap;
 	mMapDimensions = dimensions;
 }
@@ -32,13 +32,17 @@ int Map::getTileSize() {
 	return mTileSize;
 }
 
+int Map::getNumTiles() {
+	return mNumTiles;
+}
+
 // Takes a pointer to an array of integers, containing the spriteID for each tile in the Map
 // ASSUMES THE DIMENSIONS OF ARRAY spriteIDMap ARE THE SAME AS Map::mMapDimensions
 // This will have to take a depth parameter when Tile depth gets implemented
 void Map::setMapTileSpritesFromArray(int* spriteIDMap) {
-	int numTiles = mMapDimensions.x * mMapDimensions.y; // Get the number of tiles in the map
+	mNumTiles = mMapDimensions.x * mMapDimensions.y; // Get the number of tiles in the map
 
-	for (int i = 0; i < numTiles; i++) {
+	for (int i = 0; i < mNumTiles; i++) {
 		mTileArray[i].setSpriteID(spriteIDMap[i]); // Copy the spriteID to the Map at tile 'i'
 		printf("> Map::setMapTileSpritesFromArray - Setting map tile %d to spriteID %d\n", i, spriteIDMap[i]);
 	}
