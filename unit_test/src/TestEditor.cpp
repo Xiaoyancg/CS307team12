a9db8ca6458c898d77a6f3e61c9623727cf8515e
AAA <work@lixiaoyan.info>
2021-03-10 02:59:47 -0500

    editor unit test

--- unit_test/src/TestEditor.cpp
+++ unit_test/src/TestEditor.cpp
@@ -4,9 +4,23 @@ std::string SDLInitError;
 unsigned int OpenGLInitError;
 std::string menuError;
 bool isSaveAsOpen;
+bool dobreak = true;
 TEST(TEST_EDITOR, SDLInit) {
     EditorMain ( 0,  (char**)NULL );
     EXPECT_TRUE(SDLInitError.size() == 0);
 }
 
+TEST ( TEST_EDITOR, OpenGLInit )
+{
+    EditorMain ( 0, ( char ** ) NULL );
+    EXPECT_TRUE ( OpenGLInitError == GL_NO_ERROR );
+}
+
+TEST ( TEST_EDITOR, SAVE_AS )
+{
+
+    //dobreak = false;
+    EditorMain ( 0, ( char ** ) NULL );
+    EXPECT_TRUE ( isSaveAsOpen );
+}
 
