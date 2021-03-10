bce0957132027c1205ca6719e22bfd4159a15d24
AAA <work@lixiaoyan.info>
2021-03-10 11:20:03 -0500

    init class design

--- core/include/Game.h
+++ core/include/Game.h
@@ -3,6 +3,21 @@
 #include <Page.h>
 #include <glad/glad.h>
 #include <stdio.h>
+
+class Game
+{
+public:
+    Game ();
+    
+    std::string getName ();
+    int setName ();
+
+private:
+    std::vector<Page> pageList;
+    std::string name;
+};
+
+
 void sdl_die ( const char *err_msg );
 void initShaders ();
 
