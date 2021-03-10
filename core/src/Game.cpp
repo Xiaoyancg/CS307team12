a470c424b98ab6ef676e73bd2051a00392adfb5b
Josh Gerber <gerber32@purdue.edu>
2021-03-09 23:58:37 -0500

    CORE DEMO: press '1' to see a small map, '2' to see a large map, '3' for an interactive demo using 'a', 'z', and the arrow keys. Also downgraded OpenGL version

--- core/src/Game.cpp
+++ core/src/Game.cpp
@@ -13,7 +13,7 @@
 #include <test_core.h>
 #endif // __TEST_CORE
 
-SDL_Window *window;
+SDL_Window* window;
 SDL_GLContext gl_context;
 unsigned int shaderProgram;
 
@@ -26,13 +26,12 @@ int height = 720; // Height of the window, used in Entity.cpp
 
 // Use sdl_die when an SDL error occurs to print out the error and exit
 // argument err_msg can be anything, but try to keep it related to the error
-void sdl_die ( const char *err_msg )
+void sdl_die(const char* err_msg)
 {
-    printf ( "%s: %s\n", err_msg, SDL_GetError () );
-    SDL_Quit ();
-    exit ( 1 );
+	printf("%s: %s\n", err_msg, SDL_GetError());
+	SDL_Quit();
+	exit(1);
 }
-
 
 
 void initShaders() {
@@ -49,8 +48,8 @@ void initShaders() {
 		}
 	)glsl";
 
-  // Source for the fragment shader
-    const char *fragmentSource = R"glsl(
+	// Source for the fragment shader
+	const char* fragmentSource = R"glsl(
 		// fragment shader
 		#version 450 core
 
@@ -62,83 +61,83 @@ void initShaders() {
 		}
 	)glsl";
 
-  // Create vertex and fragment shaders
-    GLuint vertexShaderID, fragmentShaderID;
-    vertexShaderID = glCreateShader ( GL_VERTEX_SHADER );
-    fragmentShaderID = glCreateShader ( GL_FRAGMENT_SHADER );
+	// Create vertex and fragment shaders
+	GLuint vertexShaderID, fragmentShaderID;
+	vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
 
-    // Set the source code of the shaders
-    glShaderSource ( vertexShaderID, 1, &vertexSource, NULL );
-    glShaderSource ( fragmentShaderID, 1, &fragmentSource, NULL );
+	// Set the source code of the shaders
+	glShaderSource(vertexShaderID, 1, &vertexSource, NULL);
+	glShaderSource(fragmentShaderID, 1, &fragmentSource, NULL);
 
-    // Debug variables
-    int success;
-    char infoLog[512];
+	// Debug variables
+	int success;
+	char infoLog[512];
 
-    // Compile the vertex shader, or print an error
-    glCompileShader ( vertexShaderID );
-    glGetShaderiv ( vertexShaderID, GL_COMPILE_STATUS, &success );
-    if ( !success )
-    {
-        glGetShaderInfoLog ( vertexShaderID, 512, NULL, infoLog );
-        printf ( "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n>%s\n", infoLog );
-    };
+	// Compile the vertex shader, or print an error
+	glCompileShader(vertexShaderID);
+	glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShaderID, 512, NULL, infoLog);
+		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n>%s\n", infoLog);
+	};
 
-    // Compile the fragment shader, or print an error
-    glCompileShader ( fragmentShaderID );
-    glGetShaderiv ( fragmentShaderID, GL_COMPILE_STATUS, &success );
-    if ( !success )
-    {
-        glGetShaderInfoLog ( fragmentShaderID, 512, NULL, infoLog );
-        printf ( "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n>%s\n", infoLog );
-    };
+	// Compile the fragment shader, or print an error
+	glCompileShader(fragmentShaderID);
+	glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShaderID, 512, NULL, infoLog);
+		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n>%s\n", infoLog);
+	};
 
 
-    // Create new shader program
-    shaderProgram = glCreateProgram();
+	// Create new shader program
+	shaderProgram = glCreateProgram();
 
-    // Attach shaders to the new shader program
-    glAttachShader ( shaderProgram, vertexShaderID );
-    glAttachShader ( shaderProgram, fragmentShaderID );
+	// Attach shaders to the new shader program
+	glAttachShader(shaderProgram, vertexShaderID);
+	glAttachShader(shaderProgram, fragmentShaderID);
 
-    // Links the attached shaders in the program to their correct shader processors
-    glLinkProgram ( shaderProgram );
+	// Links the attached shaders in the program to their correct shader processors
+	glLinkProgram(shaderProgram);
 
-    // Delete the created shaders, now that they have already been linked
-    glDeleteShader ( vertexShaderID );
-    glDeleteShader ( fragmentShaderID );
+	// Delete the created shaders, now that they have already been linked
+	glDeleteShader(vertexShaderID);
+	glDeleteShader(fragmentShaderID);
 
-    // Use the newly created shader program!
-    glUseProgram ( shaderProgram );
+	// Use the newly created shader program!
+	glUseProgram(shaderProgram);
 
-    // Create Vertex Buffer Object and Vertex Array Object
-    GLuint vbo, vao;
-    glGenVertexArrays ( 1, &vao );
-    glGenBuffers ( 1, &vbo );
+	// Create Vertex Buffer Object and Vertex Array Object
+	GLuint vbo, vao;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
 
-    // Bind the VAO and VBO
-    glBindVertexArray ( vao );
-    glBindBuffer ( GL_ARRAY_BUFFER, vbo );
+	// Bind the VAO and VBO
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    // Set the format of the vertices array in opengl so it can properly read it
-    // The arguments are 
-    // 0 to denote the 'position' vertex attribute in the vertex shader
-    // 2 ints are read at a time (x, y)
-    // The the type is float (GL_INT)
-    // GL_FALSE means the data should not be normalized
-    // The size to be read in bytes (2 * sizeof(int))
-    // Offset isn't used yet since there's only one attribute in 'vertices'
-    glVertexAttribPointer ( 0, 2, GL_INT, GL_FALSE, 2 * sizeof ( int ), ( void * ) 0 );
+	// Set the format of the vertices array in opengl so it can properly read it
+	// The arguments are 
+	// 0 to denote the 'position' vertex attribute in the vertex shader
+	// 2 ints are read at a time (x, y)
+	// The the type is float (GL_INT)
+	// GL_FALSE means the data should not be normalized
+	// The size to be read in bytes (2 * sizeof(int))
+	// Offset isn't used yet since there's only one attribute in 'vertices'
+	glVertexAttribPointer(0, 2, GL_INT, GL_FALSE, 2 * sizeof(int), (void*)0);
 
-    // Enable the position vertex attribute
-    glEnableVertexAttribArray ( 0 );
+	// Enable the position vertex attribute
+	glEnableVertexAttribArray(0);
 
-    // Enable the Vertex Object Array
-    glBindVertexArray ( vao );
+	// Enable the Vertex Object Array
+	glBindVertexArray(vao);
 
 	// Set the scale based on the width and height
 	int scaleID = glGetUniformLocation(shaderProgram, "scale");
-	glUniform2f(scaleID, (float) 2 / width, (float) 2 / height);
+	glUniform2f(scaleID, (float)2 / width, (float)2 / height);
 }
 
 // This takes care of initializing everything SDL needs to begin running
@@ -148,32 +147,32 @@ void init() {
 		sdl_die("Error initializing SDL video mode :(");
 	}
 
-    // Most of this was from the sample code, I haven't looked into it yet
-    SDL_GL_SetAttribute ( SDL_GL_CONTEXT_FLAGS, 0 );
-    SDL_GL_SetAttribute ( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+	// Most of this was from the sample code, I haven't looked into it yet
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    // Use OpenGL 4.6
-    SDL_GL_SetAttribute ( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
-    SDL_GL_SetAttribute ( SDL_GL_CONTEXT_MINOR_VERSION, 6 );
+	// Use OpenGL 4.6
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
-    SDL_GL_SetAttribute ( SDL_GL_DOUBLEBUFFER, 1 );
-    SDL_GL_SetAttribute ( SDL_GL_DEPTH_SIZE, 24 );
-    SDL_GL_SetAttribute ( SDL_GL_STENCIL_SIZE, 8 );
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-    // Create the flags for creating the window
-    SDL_WindowFlags window_flags = ( SDL_WindowFlags ) ( SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI );
+	// Create the flags for creating the window
+	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 
-    // Create a new centered game window based on window_flags (1280x720 for now)
-    window = SDL_CreateWindow ( "Game Core", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, window_flags );
+	// Create a new centered game window based on window_flags (1280x720 for now)
+	window = SDL_CreateWindow("Game Core", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, window_flags);
 
-    // Create the Game opengl context
-    gl_context = SDL_GL_CreateContext ( window );
+	// Create the Game opengl context
+	gl_context = SDL_GL_CreateContext(window);
 
 	// Setup function pointers for OpenGL
 	gladLoadGL();
 
 	// Create viewport with the default w/h (same as the window size)
-	glViewport(0, 0, width, height); 
+	glViewport(0, 0, width, height);
 
 	// Create the shaders
 	initShaders();
@@ -204,16 +203,13 @@ int render() {
 }
 
 
-int coreMain ( int argc, char *argv[] )
+int coreMain(int argc, char* argv[])
 {
-    // Initialize OpenGL and necessary SDL objects
-    init();
+	// Initialize OpenGL and necessary SDL objects
+	init();
 
 	SDL_Event event;
 	int close_window = false;
-	///////////////
-	// Game loop //
-	///////////////
 
 	///////////////
 	// ENTITY TEST
@@ -223,42 +219,38 @@ int coreMain ( int argc, char *argv[] )
 	entities.emplace_back(entityTallThin);
 	Entity* entityShortWide = new Entity(glm::vec2(500, 500), glm::vec2(64, 32), 0, 0);
 	entities.emplace_back(entityShortWide);
-	Entity* entityVeryShortWide = new Entity(glm::vec2(-640, -100), glm::vec2(128, 16), 0, 0);
+	Entity* entityVeryShortWide = new Entity(glm::vec2(640, 100), glm::vec2(128, 16), 0, 0);
 	entities.emplace_back(entityVeryShortWide);
-	Entity* entityOrigin= new Entity(glm::vec2(0, 0), glm::vec2(128, 128), 0, 0);
+	Entity* entityOrigin = new Entity(glm::vec2(1000, 300), glm::vec2(128, 128), 0, 0);
 	entities.emplace_back(entityOrigin);
 	///////////////
 
 	///////////////
 	// MAP TEST
-	int spriteMap1[] = {
+	int spriteMap[] = {
 		  1,  2,  3,  4,
 		  5,  6,  7,  8,
 		 11, 22, 33, 44,
 		 55, 66, 77, 88
 	};
 
-	int spriteMap2[] = {
-		   1,   2,   3,   4,   5,
-		   5,   6,   7,   8,   9,
-		  11,  22,  33,  44,  55,
-		  55,  66,  77,  88,  99,
-		 111, 222, 333, 444, 555
-	};
-
 	Map* map1 = new Map(glm::vec2(4, 4), 64);
-	map1->setMapTileSpritesFromArray(spriteMap1);
-	Map* map2 = new Map(glm::vec2(5, 5), 50);
-	map2->setMapTileSpritesFromArray(spriteMap2);
+	map1->setMapTileSpritesFromArray(spriteMap);
+	Map* map2 = new Map(glm::vec2(16, 16), 32);
 
-	MapPage mapPage(window, map1); // Creates a MapPage with initial map 'map1'
-	// You could also use the two commented lines below to get the same effect:
-	// MapPage mapPage(window);
-	// mapPage.setMap(map1);
+	// Here are the 2 ways to make MapPages with set maps
+	MapPage mapPage1(window, map1); // Creates a MapPage with initial map 'map1'
+	MapPage mapPage2(window);
+	mapPage2.setMap(map2);
+	////////////////
 
-	// mapPage.setMap(map2); // MapPage only supports one map at a time, so this replaces map1 in mapPage with map2
+	Page* currentPage = NULL;
 
-	Page* currentPage = &mapPage;
+
+
+	///////////////
+	// Game loop //
+	///////////////
 
 	while (!close_window) {
 		// Input handling!
@@ -297,6 +289,7 @@ int coreMain ( int argc, char *argv[] )
 				int scaleBy = 3;
 
 				switch (event.key.keysym.sym) {
+					// Control Entity movement in the interactive demo
 					// Handle left arrow key
 				case SDLK_LEFT:
 					entityInteractive->setLocation(glm::vec2(loc.x - moveBy, loc.y)); // Move left
@@ -314,6 +307,7 @@ int coreMain ( int argc, char *argv[] )
 					entityInteractive->setLocation(glm::vec2(loc.x, loc.y - moveBy)); // Move down
 					break;
 
+					// Control Entity scaling in the interactive demo
 				case SDLK_a: // a key is Scale up, for now
 					entityInteractive->setScale(glm::vec2(scale.x + scaleBy, scale.y + scaleBy));
 					break;
@@ -323,29 +317,32 @@ int coreMain ( int argc, char *argv[] )
 						entityInteractive->setScale(glm::vec2(scale.x - scaleBy, scale.y - scaleBy));
 					}
 					break;
+
+					// Control demo pages. Press '1' to see map1, '2', to see map2, and '3' to see the initial interactive demo
+				case SDLK_1:
+					currentPage = &mapPage1;
+					break;
+				case SDLK_2:
+					currentPage = &mapPage2;
+					break;
+				case SDLK_3:
+					currentPage = NULL;
+					break;
 				}
-
-				break;
-
 			}
 		}
-		
-
-		
-		///////////////
-		// PAGE TEST
-		// This is just a test to make sure page rendering is correctly set up
-		//Page page(window);
-		//page.render();
-		///////////////
-		
-		//render(); // Render the Game render function
-
-		// Render the current page
-		currentPage->render();
 
 
-		// Error checking!
+		// Render the correct page
+		if (currentPage) {
+			currentPage->render();
+		}
+		else {
+			render();
+		}
+
+
+		// Error checking! This will only print out an error if one is detected each loop
 		GLenum err(glGetError());
 		if (err != GL_NO_ERROR) {
 			std::string error;
@@ -360,24 +357,21 @@ int coreMain ( int argc, char *argv[] )
 
 			printf("GL_%s\n", error.c_str());
 		}
-
-
 #ifdef __TEST_CORE
-		coreError = glGetError ();
+		coreError = glGetError();
 		break;
 #endif // __TEST_CORE
-
 	}
 
-    // Take care of deleting SDL objects and cleanly exit 
-    SDL_GL_DeleteContext ( gl_context );
-    SDL_DestroyWindow ( window );
-    SDL_Quit ();
+	// Take care of deleting SDL objects and cleanly exit 
+	SDL_GL_DeleteContext(gl_context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
-    return 0;
+	return 0;
 }
 
-int main ( int argc, char *argv[] )
+int main(int argc, char* argv[])
 {
-    return coreMain ( argc, argv );
+	return coreMain(argc, argv);
 }
