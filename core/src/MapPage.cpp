cad1ddf792490d1dcb3f6cb42182c73f918a9568
Josh Gerber <gerber32@purdue.edu>
2021-03-09 19:21:58 -0500

    COREDINATE UPDATE (haha). Anyway I updated the coordinate system of the core to use pixel values from 0<xy<viewport.xy instead of calculating and manually converting everything. Shaders are updated for this. Also Entity::CalculateCoords looks muuuchhhhh better now

--- core/src/MapPage.cpp
+++ core/src/MapPage.cpp
@@ -23,10 +23,28 @@ void MapPage::render() {
 	// Show the current context
 	SDL_GL_MakeCurrent(Page::mWindow, Page::mPageContext);
 
+	// calculate starting pos and loop through map
+	// Get 'origin'/center of map (in tiles)
+	glm::vec2 mapDims = mMap->getDimensions();
+	glm::vec2 arrayOrigin(mapDims.x / 2, mapDims.y / 2);
+
+	// Calculate the top left corner of the map using the arrayOrigin (in pixels)
+	glm::vec2 topLeftCornerPx(arrayOrigin.x * -mMap->mTileSize, arrayOrigin.y * mMap->mTileSize);
+
 	for (int i = 0; i < mMap->getNumTiles(); i++) {
 		printf("%d ", mMap->mTileArray[i].getSpriteID());
 		if ((((i + 1) % (int) mMap->getDimensions().x) == 0) && i != 0) {
 			printf("\n");
 		}
 	}
+
+	/*
+	// TODO: Draw tiles of the map!
+	// Load the data of the 'coords' buffer into the currently bound array buffer, VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(mCoords), mCoords, GL_DYNAMIC_DRAW);
+
+	// Draw the bound buffer (coords)
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	*/
 }
