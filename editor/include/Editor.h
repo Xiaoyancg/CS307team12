8c65d55bf3568705d961ffa7c0504b0473c8d4d2
AAA <work@lixiaoyan.info>
2021-03-10 02:59:47 -0500

    editor unit test

--- editor/include/Editor.h
+++ editor/include/Editor.h
@@ -1,4 +1,13 @@
 #pragma once
+#include <SDL.h>
+#include <glad/glad.h>
+
+#include <imgui.h>
+#include <imgui_impl_sdl.h>
+#include <imgui_impl_opengl3.h>
+#include <imfilebrowser.h>
+#include <stdio.h>
+#include <iostream>
 
 int EditorMain ( int argc, char *argv[] );
 
--- unit_test/include/TestEditor.h
+++ unit_test/include/TestEditor.h
@@ -6,7 +6,7 @@ extern std::string SDLInitError;
 extern unsigned int OpenGLInitError;
 //extern std::string ImguiInitError;
 
-
+extern bool dobreak;
 extern std::string menuError;
 
 extern bool isSaveAsOpen;
