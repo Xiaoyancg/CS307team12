e4f7d8cbd2a978db738cd3b12de91bb0623718b3
AAA <work@lixiaoyan.info>
2021-03-10 21:32:39 -0500

    fix

--- core/include/Page.h
+++ core/include/Page.h
@@ -9,21 +9,28 @@
 // Page class
 // Pages can be menu, settings, HUD, map, Cutscene, ...
 // Each page contains its own context and render function
-class Page {
+class Page
+{
 
 public:
-	// Constructor, just creates the context of the page
-	Page(SDL_Window* window);
+    // Constructor, just creates the context of the page
+    Page ( SDL_Window *window );
 
-	// Render the given page on the current context
-	// Returns non-zero on error
-	int render();
-	int SetName ( std::string name );
-	std::string GetName ();
-	std::vector<Entity> getEntityList ();
+    // Render the given page on the current context
+    // Returns non-zero on error
+    int render ();
+
+    int SetName ( std::string name );
+    std::string GetName ();
+
+    std::vector<Entity> getEntityList ();
+    // return the entity id
+    int CreateEntity ();
+
+
 private:
-	SDL_GLContext mPageContext; // The page context
-	SDL_Window* mWindow; // The window the page context is related to
-	std::string name;
-	std::vector<Entity> entityList;
+    SDL_GLContext mPageContext; // The page context
+    SDL_Window *mWindow; // The window the page context is related to
+    std::string name;
+    std::vector<Entity> entityList;
 };
