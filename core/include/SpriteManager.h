8021c53b922aad52438a175d3f932772363f9fac
Josh Gerber <gerber32@purdue.edu>
2021-03-18 16:55:34 -0400

    Comments and function renaming

--- core/include/SpriteManager.h
+++ core/include/SpriteManager.h
@@ -4,22 +4,31 @@
 #include "Sprite.h"
 
 
+/*
+ * Use the SpriteManager when using sprites
+ * 
+ * This will handle constant-time access of sprites based on their ID ( using SpriteManager::atID(id) )
+ * as well as set the each sprite ID automatically. 
+ */
+
 namespace Core
 {
-	// Sprite class
+	// SpriteManager class
 	class SpriteManager
 	{
 	public:
 		// Creates a new sprite based on the filename and returns the new sprite's ID
-		int addSprite(std::string filename); 
+		int createSprite(std::string filename); 
 
 		// Returns a pointer to the sprite with the requested ID
 		// (in constant time because we're using unordered_maps instead of vectors!)
-		Sprite* at(int spriteID);
+		Sprite* atID(int spriteID);
 
 	private:
-		int mCurrSpriteID;
+		int mCurrSpriteID; // The current sprite ID, which will be set to whatever sprite is created next
 
-		std::unordered_map<int, Sprite*> mSprites;
+		// This is where sprites are stored, like an array based on their IDs
+		// If a sprite has ID=32, then a pointer to it is stored at mSprites[32]
+		std::unordered_map<int, Sprite*> mSprites; 
 	};
 }
