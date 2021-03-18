d7eadfa5fa3762142b7f373c8905bdcd119d0edc
XiaoyanLL <li2185@purdue.edu>
2021-03-17 20:15:39 -0400

    fix constructor error in unittest

--- unit_test/src/TestVM.cpp
+++ unit_test/src/TestVM.cpp
@@ -2,59 +2,57 @@
 
 #include "gtest/gtest.h"
 
-TEST ( TEST_VM, ReadFile )
+TEST(TEST_VM, ReadFile)
 {
-    std::string s ( ReadFile ( std::string ( "D:\\2021\\307\\307project\\CS307team12\\core\\CoreMain.cpp" ) ) );
-    EXPECT_TRUE ( s.size () > 0 );
+    std::string s(ReadFile(std::string("D:\\2021\\307\\307project\\CS307team12\\core\\CoreMain.cpp")));
+    EXPECT_TRUE(s.size() > 0);
 }
 
-
-
-TEST ( TEST_VM, WriteFile )
+TEST(TEST_VM, WriteFile)
 {
-    EXPECT_TRUE ( !WriteFile ( "testFile.txt", "content\ncontent2" ) );
+    EXPECT_TRUE(!WriteFile("testFile.txt", "content\ncontent2"));
 }
 
-TEST ( TEST_VM, WriteReadFile )
+TEST(TEST_VM, WriteReadFile)
 {
-    WriteFile ( "TestFile.txt", "content1\ncontent2" );
-    std::string s ( ReadFile ( std::string ( "TestFile.txt" ) ) );
-    printf ( "%s\n", s.c_str () );
+    WriteFile("TestFile.txt", "content1\ncontent2");
+    std::string s(ReadFile(std::string("TestFile.txt")));
+    printf("%s\n", s.c_str());
     // write file will write a new line at the begining
-    EXPECT_TRUE ( s.compare ( "\ncontent1\ncontent2" ) );
+    EXPECT_TRUE(s.compare("\ncontent1\ncontent2"));
 }
 
-TEST ( TEST_VM, ProduceDataFile )
+TEST(TEST_VM, ProduceDataFile)
 {
-    Core::Game g = Core::Game ();
-    g.SetGameName ( "name" );
-    EXPECT_TRUE ( !ProduceDataFile ( &g ) );
+    Core::Game g = Core::Game("empty");
+    g.SetGameName("name");
+    EXPECT_TRUE(!ProduceDataFile(&g));
 }
 
-TEST ( TEST_VM, ConstructGame )
+TEST(TEST_VM, ConstructGame)
 {
-    Core::Game * g = ConstructGame ( "name.gdata" );
-    EXPECT_TRUE ( !std::string ( g->getGameName () ).compare ( "name" ) );
+    Core::Game *g = ConstructGame("name.gdata");
+    EXPECT_TRUE(!std::string(g->getGameName()).compare("name"));
     delete g;
 }
 
-TEST ( TEST_VM, ProduceDataFileConstructGame )
+TEST(TEST_VM, ProduceDataFileConstructGame)
 {
-    Core::Game * g = new Core::Game ( std::string ( "huh" ) );
-    ProduceDataFile ( g );
-    Core::Game * r = ConstructGame ( std::string ( "huh.gdata" ) );
+    Core::Game *g = new Core::Game(std::string("huh"));
+    ProduceDataFile(g);
+    Core::Game *r = ConstructGame(std::string("huh.gdata"));
 
-    EXPECT_STREQ ( r->getGameName ().c_str(), "huh" );
+    EXPECT_STREQ(r->getGameName().c_str(), "huh");
     delete g;
     delete r;
 }
 
-TEST ( TEST_VM, RealDataFileTest)
+TEST(TEST_VM, RealDataFileTest)
 {
-    Core::Game *g = CreateExampleGame ();
-    ProduceDataFile ( g );
-    Core::Game *r = ConstructGame ( std::string("example.gdata") );
-    EXPECT_STREQ ( r->getGameName ().c_str (), "example" );
+    Core::Game *g = CreateExampleGame();
+    ProduceDataFile(g);
+    Core::Game *r = ConstructGame(std::string("example.gdata"));
+    EXPECT_STREQ(r->getGameName().c_str(), "example");
 }
 
 /*
