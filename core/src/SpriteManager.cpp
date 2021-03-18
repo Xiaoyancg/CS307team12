51a0d60d2d3a6707609ca0cd35a093d1d192ec3b
Josh Gerber <gerber32@purdue.edu>
2021-03-18 16:55:34 -0400

    Comments and function renaming

--- core/src/SpriteManager.cpp
+++ core/src/SpriteManager.cpp
@@ -2,12 +2,14 @@
 
 namespace Core
 {
-	int SpriteManager::addSprite(std::string filename) {
-		mSprites[mCurrSpriteID] = new Sprite(filename);
-		mCurrSpriteID++; // Increment the sprite ID so no sprite gets a duplicate ID
+	// Creates a new sprite at the correct location in mSprites 
+	int SpriteManager::createSprite(std::string filename) {
+		this->mSprites[mCurrSpriteID] = new Sprite(filename);
+		this->mCurrSpriteID++; // Increment the sprite ID so no sprite gets a duplicate ID
 	}
 
-	Sprite* SpriteManager::at(int spriteID) {
+	// Returns to a pointer to the sprite with the requested ID
+	Sprite* SpriteManager::atID(int spriteID) {
 		return mSprites[spriteID];
 	}
 
