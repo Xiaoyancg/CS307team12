e6b4f0f4082506c6533f557d074a9ff45b9161da
XiaoyanLL <li2185@purdue.edu>
2021-03-21 12:10:24 -0400

    fix vec2 to int and add logic.h

---
+++ core/include/Logic.h
@@ -0,0 +1,19 @@
+
+// logic class contains all the information needed to achieve the designed result
+class Logic
+{
+private:
+    /* data */
+
+public:
+    Logic(/* args */);
+    ~Logic();
+};
+
+Logic::Logic(/* args */)
+{
+}
+
+Logic::~Logic()
+{
+}
