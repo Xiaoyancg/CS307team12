36ba7720df9ab6c0b4f25f3cc5ddcb8fcaafa18d
XiaoyanLL <li2185@purdue.edu>
2021-03-27 13:52:10 -0400

    tried demo

--- core/src/Response.cpp
+++ core/src/Response.cpp
@@ -1,5 +1,8 @@
 #include "Response.h"
 namespace Core
 {
-
+    bool KeyResponse::keyResponse(SDL_KeyboardEvent event)
+    {
+        return event.keysym.sym == mkey && event.type == mtype;
+    }
 }
