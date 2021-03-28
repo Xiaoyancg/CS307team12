acb3853a0795475b96b4f5f33eb227d996b6fd35
dlocasc <dlocasc@purdue.edu>
2021-03-27 22:32:57 -0400

    Yeah we mergin'

--- core/src/Game.cpp
+++ core/src/Game.cpp
@@ -38,6 +38,12 @@ namespace Core
         setupSpriteRefs();
     }
 
+    Game::Game(nlohmann::json& json)     {
+        this->parse(json);
+        useFramebuffer = false;
+        setupSpriteRefs();
+    }
+
     // editor new
     Game::Game(GLuint *o)
     {
