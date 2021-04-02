f22c3ba255d57f945858afe4a4d15b38ec457f2e
Drew <hatfielddr>
2021-04-02 04:13:44 -0400

    changed listbox height to 8 instead lmao

--- editor/src/Editor.cpp
+++ editor/src/Editor.cpp
@@ -662,7 +662,7 @@ static void ShowExampleAppMainMenuBar()
             ImGui::Text("Select Entity:");
             char **entities_list = (char **)malloc(sizeof(char *) * currPage->getEntityList().size());
             static int current_entity = 0;
-            if (ImGui::BeginListBox("", ImVec2(200, 10 * ImGui::GetTextLineHeightWithSpacing())))
+            if (ImGui::BeginListBox("", ImVec2(200, 8 * ImGui::GetTextLineHeightWithSpacing())))
             {
                 // Set default selected entity to be the first in the entity list
                 if (currentComponent[CUR_ENTITY] == "No Component Selected" && currPage->getEntityList().size() > 0)
