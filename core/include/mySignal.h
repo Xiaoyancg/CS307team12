435a60108654aff1258f3ba51fbda6c7bb27c8d3
XiaoyanLL <li2185@purdue.edu>
2021-04-05 08:15:59 -0400

    signal done with mockup ui editor

--- core/include/mySignal.h
+++ core/include/mySignal.h
@@ -67,13 +67,13 @@ namespace Core
         ~SignalKey();
     };
 
-    //* ----------------- ANCHOR SIGNAL UNION ---------------- *//
+    //* -------------------- SIGNAL UNION -------------------- *//
 
     union SignalUnion
     {
         SignalKey keySignal;
         SignalCustom customSignal;
-
+        SignalUnion &operator=(const SignalUnion &other);
         SignalUnion();
         SignalUnion(const SignalUnion &);
         SignalUnion(SignalKey keySignal);
@@ -81,7 +81,7 @@ namespace Core
         ~SignalUnion();
     };
 
-    //* ----------------------- SIGNAL ----------------------- *//
+    //* -------------------- ANCHOR SIGNAL ------------------- *//
 
     /// \brief the signal union contains all kind of signal and id and type
     ///
@@ -91,22 +91,49 @@ namespace Core
         int _signalId;
         SignalType _signalType;
         SignalUnion _signal;
+        std::string _signalName;
+
+        int getSignalId();
+        void setSignalId(int signalId);
+        SignalType getSignalType();
+        void setSignalType(SignalType signalType);
+        std::string getSignalName();
+        void setSignalName(std::string signalName);
+        SignalUnion getSignal();
+        void setSignal(SignalUnion signal);
 
     public:
+        /// \brief Parse function
+        ///
+        /// \param root
+        /// \return Signal
         static Signal parse(nlohmann::json root);
 
-        /// \brief Construct a new Signal object by default.
-        /// Id is -1, type is custom.
+        /// \brief update all information of Signal
+        /// *For editor, binding with the update button in signal editor
         ///
-        Signal() : Signal(-1, SignalType::Custom, SignalUnion(SignalCustom())) {}
-        Signal(const Signal &);
+        /// \param signalType
+        /// \param ... id, name, rest are signal variable. List of different type:
+        /// \param Custom: std::vector<int>
+        ///
+        void updateSignal(SignalType signalType, ...);
+
+        /// \brief Construct a new Signal object by default.
+        /// Id is -1, type is custom, name is "".
+        /// *For editor, used in Game::createSignal() binding with create button
+        /// in signal editor
+        ///
+        Signal();
+
         /// \brief Construct a new Signal object
         ///
         /// \param signalId
         /// \param signalType
         /// \param signal
-        Signal(int signalId, SignalType signalType, SignalUnion signal);
+        Signal(int signalId, SignalType signalType, std::string signalName,
+               SignalUnion signal);
 
+        Signal(const Signal &);
         ~Signal();
     };
 
