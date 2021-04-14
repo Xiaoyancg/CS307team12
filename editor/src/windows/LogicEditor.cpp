d42c4ec04bb40e521ab2277bd1990f0f3f6ec40c
Raymond Xie <raymond.xie.rx@gmail.com>
2021-04-14 19:55:27 -0400

    Set up basic UI of logic editor

--- editor/src/windows/LogicEditor.cpp
+++ editor/src/windows/LogicEditor.cpp
@@ -1,6 +1,8 @@
 #include "windows/LogicEditor.h"
 #include "UndoRedo.h"
 
+// TODO: Make logic ID input turn all negative IDs to -1?
+
 void LogicEditor::draw()
 {
     if (visible)
@@ -9,41 +11,69 @@ void LogicEditor::draw()
         ImGui::SetNextWindowSize(size, ImGuiCond_FirstUseEver);
 
         static char logic_name[128] = "";
-        static char logicIDInput[128] = "";
+        static int logicIDInput = -1;
         logicInfo = false;
         if (ImGui::Begin("Logic Editor", &visible))
         {
-            /*ImGui::Text("Enter Logic Name:");
+            // Set input widget widths to 200
             ImGui::PushItemWidth(200);
-            ImGui::InputText(" ", logic_name, IM_ARRAYSIZE(logic_name));
-            ImGui::SameLine();
-            ImGui::Text("Enter ID:");
-            ImGui::InputText(" ", logicIDInput, IM_ARRAYSIZE(logicIDInput));
-            ImGui::SameLine();
-            if (ImGui::BeginCombo("Type", "key"))
-            {
-                ImGui::Selectable("test");
-                ImGui::Selectable("key");
-                ImGui::Selectable("lol");
-            }*/
 
+            ImGui::Text("Logic Name:");
+            ImGui::SameLine();
+            ImGui::InputText("##logic_name", logic_name, IM_ARRAYSIZE(logic_name));
+
+            ImGui::Text("ID:        ");
+            ImGui::SameLine();
+            ImGui::InputInt("##logic_id", &logicIDInput);
+
+            ImGui::Text("Type:      ");
+            ImGui::SameLine();
+            const char* items[] = { "Key", "Custom" };
+            static int item_current_idx = 0; // Here we store our selection data as an index.
+            const char* combo_label = items[item_current_idx];
+            if (ImGui::BeginCombo("##logic_type", items[item_current_idx]))
+            {
+                for (int n = 0; n < IM_ARRAYSIZE(items); n++)
+                {
+                    const bool is_selected = (item_current_idx == n);
+                    if (ImGui::Selectable(items[n], is_selected))
+                        item_current_idx = n;
+
+                    // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                    if (is_selected)
+                        ImGui::SetItemDefaultFocus();
+                }
+                ImGui::EndCombo();
+            }
+
+            ImGui::PopItemWidth();
 
             ImGui::Text("Current Logic: ");
-            if (ImGui::BeginListBox("", ImVec2(200, 8 * ImGui::GetTextLineHeightWithSpacing())))
+            if (ImGui::BeginListBox(" ", ImVec2(200, 8 * ImGui::GetTextLineHeightWithSpacing())))
             {
                 ImGui::EndListBox();
             }
-
-            if (ImGui::Button("Delete Logic"))
-            {
-            
-            }
-
             if (ImGui::Button("Show Information"))
             {
                 logicInfo = true;
             }
 
+            if (ImGui::Button("Create"))
+            {
+
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("Update"))
+            {
+
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("Delete"))
+            {
+            
+            }
+
+            // Open logic info popup when "Show Information" is clicked
             if (logicInfo)
             {
                 ImGui::OpenPopup("Logic Information");
