e5d6a0f16b5d42d95c8c6cd9b15527e34d76815c
Drew <hatfielddr>
2021-04-17 17:32:43 -0400

    missing files

---
+++ editor/include/HelpMarker.h
@@ -0,0 +1,14 @@
+#pragma once
+
+void HelpMarker(const char* desc)
+{
+    ImGui::TextDisabled("(?)");
+    if (ImGui::IsItemHovered())
+    {
+        ImGui::BeginTooltip();
+        ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+        ImGui::TextUnformatted(desc);
+        ImGui::PopTextWrapPos();
+        ImGui::EndTooltip();
+    }
+}
