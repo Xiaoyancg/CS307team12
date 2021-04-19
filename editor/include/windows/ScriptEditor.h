18b2235dd4c1e907b3d4d2ec79b3e5d861f3d373
Jacob Brown <brow1623@purdue.edu>
2021-04-18 20:10:57 -0400

    signal editor view, script editor adjustments, other additions

--- editor/include/windows/ScriptEditor.h
+++ editor/include/windows/ScriptEditor.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include "windows/Window.h"
+#include <string>
 
 class ScriptEditor : public Window
 {
 public:
-	DEFAULT_CONSTRUCTOR(ScriptEditor) {}
-	void draw();
+    DEFAULT_CONSTRUCTOR(ScriptEditor)
+    {
+        scriptName[0] = '\0';
+    }
+    void draw();
 
 private:
 	int currScriptId = 0;
+	char scriptName[128];
+	bool scriptInfo;
 };
