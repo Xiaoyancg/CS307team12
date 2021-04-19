fea22ace78a41ed27244a43f7d8472b641244433
dlocasc <dlocasc@purdue.edu>
2021-04-19 13:33:20 -0400

    game view resizing and centering

--- editor/src/windows/GameWindow.cpp
+++ editor/src/windows/GameWindow.cpp
@@ -9,7 +9,7 @@ void GameWindow::draw()
         {
             // set the windows default size (not needed for game window)
             //ImGui::SetNextWindowSize(size, ImGuiCond_FirstUseEver);
-            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+            //ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
 
             // the game view window itself
             ImGui::Begin("Game View", &visible);
@@ -28,19 +28,30 @@ void GameWindow::draw()
                 lastTime = now;
                 editor->getGamePtr()->update(dt);
             }
-
-            // Get size of drawable space on the window, instead of the entire size of the window
-            ImVec2 canvas_size = ImGui::GetContentRegionAvail();
+            
+            ImGui::Separator();
 
             glViewport(0, 0, editor->getGamePtr()->width, editor->getGamePtr()->height); // Set viewport to the Game dimensions
             glBindFramebuffer(GL_FRAMEBUFFER, mFBO);
             editor->getGamePtr()->render(); // Render Game with new viewport size
             glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-            ImGui::Image((ImTextureID) mTexCBO, ImVec2(editor->getGamePtr()->width, editor->getGamePtr()->height), ImVec2(0, 1), ImVec2(1, 0));
+            ImVec2 windowSize = ImGui::GetWindowSize();
+            float aspectRatio = (float) editor->getGamePtr()->width / editor->getGamePtr()->height;
+            ImVec2 cursor = ImGui::GetCursorPos();
+            cursor.y += 10;
+            float gameHeight = windowSize.y - cursor.y - 10;
+            float gameWidth = gameHeight * aspectRatio;
+            if (gameWidth + 20 > windowSize.x) {
+                gameWidth = windowSize.x - 20;
+                gameHeight = gameWidth / aspectRatio;
+            }
+
+            cursor.x = (windowSize.x - gameWidth) / 2;
+            ImGui::SetCursorPos(cursor);
+            ImGui::Image((ImTextureID) mTexCBO, ImVec2(gameWidth, gameHeight), ImVec2(0, 1), ImVec2(1, 0));
 
             ImGui::End();
-            ImGui::PopStyleVar();
         }
     }
 }
