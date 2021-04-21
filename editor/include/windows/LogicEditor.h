1bab2ef09ee5b731cf4c4ef688ebe4020ee63ece
XiaoyanLL <li2185@purdue.edu>
2021-04-21 03:34:55 -0400

    keycode working

--- editor/include/windows/LogicEditor.h
+++ editor/include/windows/LogicEditor.h
@@ -2,10 +2,15 @@
 
 #include "windows/Window.h"
 
-class LogicEditor : public Window {
+class LogicEditor : public Window
+{
 public:
     DEFAULT_CONSTRUCTOR(LogicEditor) {}
     void draw();
+
 private:
     bool logicInfo = false;
+    char keyCodeString[20] = "";
+    SDL_Keycode keyCode;
+    Uint32 keyType = 0;
 };
