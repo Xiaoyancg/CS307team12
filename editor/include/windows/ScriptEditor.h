14445ff0203fc0a5f3f26e2cd864f6ca56297c0c
XiaoyanLL <li2185@purdue.edu>
2021-04-20 20:20:43 -0400

    script editor custom done

--- editor/include/windows/ScriptEditor.h
+++ editor/include/windows/ScriptEditor.h
@@ -13,8 +13,27 @@ public:
     void draw();
 
 private:
-    int currScriptIdx = 0;
-    Core::Script *currScript_ptr;
+    void customWidgets();
+    void getInfo(Core::Script *);
+    void listTreeNode(std::string, int);
+    int currScriptIdx = -1;
+    Core::Script *currScript_ptr = nullptr;
     char scriptName[128];
-    bool scriptInfo;
+    bool scriptInfo = false;
+    // info
+    int scriptId = -1;
+    int type = 0;
+    const char *typeNameList[1] = {"Custom"}; //, "MoveConstantly"};
+    std::vector<int> addTargetSignalList;
+    int newAddSignal = 0;
+    std::vector<int> addTargetLogicList;
+    int newAddLogic = 0;
+    std::vector<int> addTargetScriptList;
+    int newAddScript = 0;
+    std::vector<int> removeTargetSignalList;
+    int newRemoveSignal = 0;
+    std::vector<int> removeTargetLogicList;
+    int newRemoveLogic = 0;
+    std::vector<int> removeTargetScriptList;
+    int newRemoveScript = 0;
 };
