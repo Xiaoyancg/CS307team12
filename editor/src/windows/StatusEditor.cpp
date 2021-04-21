17c1da275dd9fd3f7b107d45be2f1be61898d8c2
Jacob Brown <brow1623@purdue.edu>
2021-04-21 01:20:34 -0400

    state editor is now status editor

--- editor/src/windows/StatusEditor.cpp
+++ editor/src/windows/StatusEditor.cpp
@@ -1,24 +1,24 @@
-#include "windows/StateEditor.h"
+#include "windows/StatusEditor.h"
 #include "UndoRedo.h"
 #include "HelpMarker.h"
 
-void StateEditor::draw() {
+void StatusEditor::draw() {
     if (visible)
     {
         // possibly implement a new function here for readability purposes
         // set the windows default size
         ImGui::SetNextWindowSize(size, ImGuiCond_FirstUseEver);
-        static char state_name[128] = "";
+        static char status_name[128] = "";
         static bool on_collision = false;
         static bool on_property_change = false;
-        if (ImGui::Begin("State Editor", &visible))
+        if (ImGui::Begin("Status Editor", &visible))
         {
             if (ImGui::Button("Create Score Tracker"))
             {
 
             }
             ImGui::SameLine();
-            HelpMarker("A score tracker allows for in-game scores to be updated based on event outcomes.");
+            HelpMarker("A score tracker allows for a score to be maintained and updated based on the outcomes of in-game events.");
             if (ImGui::Button("Create Entity Property"))
             {
 
@@ -26,12 +26,12 @@ void StateEditor::draw() {
             ImGui::SameLine();
             HelpMarker("Entity properties include characteristics such as health and speed that can be affected by in-game events.");
             ImGui::NewLine();
-            ImGui::Checkbox("Change State On Collision", &on_collision);
+            ImGui::Checkbox("Change Status On Collision", &on_collision);
             ImGui::SameLine();
-            HelpMarker("Update a state if a collision between two entities occurs.");
-            ImGui::Checkbox("Change State On Existing State Change", &on_property_change);
+            HelpMarker("Update a status if a collision between two entities occurs.");
+            ImGui::Checkbox("Change Status On Existing Status Change", &on_property_change);
             ImGui::SameLine();
-            HelpMarker("Update an existing state based on the outcome of another state (e.g. change the game's score if an entity's health is depleted).");
+            HelpMarker("Update an existing status based on the outcome of another status (e.g. change the game's score if an entity's health is depleted).");
         }
         ImGui::End();
     }
